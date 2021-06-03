df1ea5dd0b2f4f5eeeb590f07a0d4312d2b78eb9
Amir Ghamarian <amir@machine2learn.nl>
2021-06-03 20:45:51 +0200

    Adds readme.md

--- get_data.py
+++ get_data.py
@@ -9,7 +9,7 @@ if __name__ == '__main__':
     for coin in config.SUPPORTED_COIN_LIST:
         data_manager.get_all_binance_in_csv(coin, '1m', True)
 
-    data_manager.update_all_symbols()
-
-    df = data_manager.load_from_db_for_all('minutely', '*', ['DOGEUSDT', 'ATOMUSDT', 'ETCUSDT'], '2020-07-01', '2020-08-01')
-    print(df.head())
+    # data_manager.update_all_symbols()
+    #
+    # df = data_manager.load_from_db_for_all('minutely', '*', ['DOGEUSDT', 'ATOMUSDT', 'ETCUSDT'], '2020-07-01', '2020-08-01')
+    # print(df.head())
