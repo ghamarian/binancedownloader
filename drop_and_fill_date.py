db0bc9e595bd36146b3df217cb506f29c023149f
hx65yj <hamed.ghasemieh@ing.com>
2021-11-11 21:13:07 +0100

    fill gaps and drop duplicates

---
+++ drop_and_fill_date.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import glob
+import os
+
+for fname in glob.glob(os.path.join('../data', '*.csv')):
+    df = pd.read_csv(fname)
+    print(fname)
+    print(len(df))
+    df['date'] = pd.to_datetime(df.date)
+    ddf = (df
+           .drop_duplicates('date')
+           .set_index('date')
+           .asfreq(freq='1Min')
+           .interpolate('linear', axis=0)
+           .fillna(method='bfill')
+           )
+    print(len(ddf))
+    ddf.to_csv(os.path.join('../data_2', fname.split(os.sep)[-1]), index=False)
